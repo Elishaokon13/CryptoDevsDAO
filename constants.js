53da51861527a95441fb762586621fcccd7d6154
Elisha Okon <63073316+Elishaokon13@users.noreply.github.com>
2022-08-20 17:53:59 +0100

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "YOUR_CRYPTODEVS_NFT_CONTRACT_ADDRESS_HERE";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
