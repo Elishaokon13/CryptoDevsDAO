5e76436d5cbf9f70cffb2b99799aa41d8f471716
Elishaokon13 <eliscodavid@gmail.com>
2022-08-21 05:17:49 +0100

    env

--- CryptoDevsDAO/constants.js
+++ CryptoDevsDAO/constants.js
@@ -2,4 +2,69 @@ export const CRYPTODEVS_DAO_CONTRACT_ADDRESS = "";
 export const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "";
 
 export const CRYPTODEVS_DAO_ABI = [];
-export const CRYPTODEVS_NFT_ABI = [];
+export const CRYPTODEVS_NFT_ABI = [
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_tokenId",
+                "type": "uint256"
+            }
+        ],
+        "name": "available",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getPrice",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_tokenId",
+                "type": "uint256"
+            }
+        ],
+        "name": "purchase",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "tokens",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
